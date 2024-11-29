d228c426087f1af317a7567573a3f90d3e26a652
Bhavika <bhavikajanday@gmail.com>
2024-11-29 18:20:39 +0530

    app

--- api.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-export default function handler(req, res) {
-    if (req.method === 'GET') {
-        res.sendFile(path.join(__dirname, '..', 'public', 'try.html'));
-    }
-}
